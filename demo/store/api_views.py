dc02f11f71928ee607fa2844b1ba6743ddd0c781
sd8917 <sudhanshuraj8917@gmail.com>
2020-06-11 00:38:26 +0530

    initial commit

--- demo/store/api_views.py
+++ demo/store/api_views.py
@@ -2,14 +2,17 @@ from store.serializers import ProductSerializer
 from store.models import Product
 from rest_framework import generics
 from rest_framework.filters import SearchFilter
+
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import ListAPIView,CreateAPIView
 # #this add filter a buuton
 
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.pagination import LimitOffsetPagination
 
-# class ProductPagination(LimitOffsetPagination):
-#     default_limit = 3
-#     max_limit = 100
+class ProductPagination(LimitOffsetPagination):
+    default_limit = 3
+    max_limit = 100
 
 
 class ProductList(generics.ListAPIView):
@@ -17,9 +20,9 @@ class ProductList(generics.ListAPIView):
     serializer_class = ProductSerializer
     filter_backends = (DjangoFilterBackend, SearchFilter)
     filter_fields = ('id',)
-#     search_fields = ('name', 'description', )
+    search_fields = ('name', 'description', )
 
-#     pagination_class = ProductPagination
+    pagination_class = ProductPagination
 
 #     #overrite the method to get that whether the product is on sell or not
 
@@ -40,3 +43,19 @@ class ProductList(generics.ListAPIView):
             )
 
         return queryset
+
+
+class ProductCreate(CreateAPIView):
+    serializer_class = ProductSerializer
+
+    def create(self, request, *args, **kwargs):
+
+        try:
+            price = request.data.get('price')
+
+            if price is not None and float(price) <= 0.0:
+                raise ValidationError({'price': 'price must be above $0.0'})
+
+
+        except ValueError:
+            raise ValidationError({'price': 'A value number is required '})
